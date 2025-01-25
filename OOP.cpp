95423406ba2749334783ed90777ce5e921f16552
Hassan-Raza-Shaikh <ammarbatman9@gmail.com>
2025-01-25 15:57:59 +0500

    .

---
+++ OOP.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+struct coordinates 
+{
+    int x,y;
+};
+
+int main()
+{
+    coordinates point1;    // this could also be written like struct coordinates point1; , again struct keyword in main is not necessary
+    coordinates point2;    // instance can be one or many, it can also be an array
+
+    point1.x=0;
+    point1.y=0;
+    point2.x=5;
+    point2.y=5;
+
+    cout<<"Point 1 coordinates are : ("<<point1.x<<","<<point1.y<<")"<<endl;
+    cout<<"Point 2 coordinates are : ("<<point2.x<<","<<point2.y<<")"<<endl;
+}
