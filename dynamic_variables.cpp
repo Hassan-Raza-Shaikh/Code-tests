ae35aaad552c2e724b440011e5590dac6401e2b8
Hassan-Raza-Shaikh <ammarbatman9@gmail.com>
2025-04-25 10:15:34 +0500

    .

---
+++ dynamic_variables.cpp
@@ -0,0 +1,27 @@
+// bss memory allocation- block started by symbol (static variables)- shareable
+// stack memory allocation
+// dynamic memory allocation
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int x=10;
+    // int *y=&x;
+    // delete y; // Error
+    // cout<<"x "<<*y;
+
+    // int x=10;
+    // int y=12;
+    // delete x; // Error: delete operator cannot be applied to non-dynamic memory
+    // cout<<"x "<<x;
+
+    //pointer is not deleted, the address it is pointing to is removed
+
+// dynamic memory allocation order in class inheritance is same as general hierachy
+
+    // process is dynamic, array is not dynamic 
+    
+    return 0;
+}
